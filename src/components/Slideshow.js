d2212ded5923e4b0509110d56d615b6567e1927d
Stephanie Denny <stephanie_denny@volusion.com>
2019-11-28 22:34:48 -0600

    update cms config

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -8,27 +8,24 @@ export default class Slideshow extends React.Component {
 
   render() {
 
-
-const { fadeImages } = this.props
-  return (
-    <Carousel>
-      {!!fadeImages &&
-          fadeImages.map((slide, index) => (
-  <Carousel.Item key={index}>
-    <img
-      className="d-block w-100"
-      src={slide.image}
-      alt={slide.title}
-    />
-    <Carousel.Caption>
-      <h3>{slide.title}</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-
-
-  ))}
-  </Carousel>
-  )
-}
+  const { fadeImages } = this.props
+    return (
+      <Carousel>
+        {!!fadeImages &&
+            fadeImages.map((slide, index) => (
+            <Carousel.Item key={index}>
+              <img
+                className="d-block w-100"
+                src={slide.image}
+                alt={slide.title}
+              />
+              <Carousel.Caption>
+                <h3>{slide.title}</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+        ))}
+      </Carousel>
+    )
+  }
 }
